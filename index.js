3d2a8f072c5f729d6ec063d9811a9fab8817a71b
Chirag <cgupta7.rag2101@gmail.com>
2021-08-11 21:05:52 +0530

    keypress sensitive

--- index.js
+++ index.js
@@ -1,6 +1,48 @@
+document.addEventListener("keydown",function(event){
+
+    // var whichKey=event.key;
+    // alert(event.key);
+    // console.log(whichKey);
+    switch (event.key) {
+        case "w":
+            var audio = new Audio('sounds/crash.mp3');
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio('sounds/kick-bass.mp3');
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio('sounds/snare.mp3');
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio('sounds/tom-1.mp3');
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio('sounds/tom-2.mp3');
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio('sounds/tom-3.mp3');
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio('sounds/tom-4.mp3');
+            audio.play();
+            break;
+    
+        default:
+            break;
+        }
+        
+    
+});
+
+
 for(var i=0;i<document.querySelectorAll(".drum").length;i++)
 {
-    // var audiopPlayer= "audio"+i+".play();"
 document.querySelectorAll(".drum")[i].addEventListener("click",handleClick);
 }
 
@@ -40,5 +82,7 @@ function handleClick(){
         default:
             break;
     }
-     
+   
 }
+
+ 
